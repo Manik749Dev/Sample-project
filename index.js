abbb53c5de9af2d989359f36241d41557e793e00
apnacollage <Manik@antiersolution.com>
2025-11-04 17:45:18 +0530

    text added

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const app = express();
 
-// Create a basic route
 app.get("/", (req, res) => {
-  // res.send("Hello! Your local server is running successfully 🚀");
-  res.send("server is running successfully 🚀");
-            
+  res.send("This is the NEW updated text 🚀");
 });
 
-// Run the server
 app.listen(3000, () => {
   console.log("Server is running at http://localhost:3000");
 });
